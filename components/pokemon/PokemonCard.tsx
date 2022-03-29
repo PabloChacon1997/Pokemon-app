c54f9d81b1e1006731f694328215952777270402
PabloChacon1997 <pabloandres120297@gmail.com>
2022-03-29 00:18:47 -0500

    Pokemon static

---
+++ components/pokemon/PokemonCard.tsx
@@ -0,0 +1,43 @@
+import { FC } from "react"
+import { useRouter } from 'next/router';
+import { Card, Grid, Row, Text } from '@nextui-org/react';
+
+import { SmallPokemon } from "../../interfaces"
+
+
+interface Props {
+  pokemon: SmallPokemon
+}
+
+export const PokemonCard: FC<Props> = ({ pokemon }) => {
+  const { id, imagen, name } = pokemon;
+
+  const router = useRouter();
+
+  const onClick =() => {
+    router.push(`/pokemon/${id+ 1}`);
+  }
+
+  return (
+    <Grid xs={6} sm={3} md={2} xl={1} key={id}>
+      <Card
+        onClick={onClick} 
+        hoverable 
+        clickable>
+        <Card.Body css={{p: 1}}>
+          <Card.Image 
+            src={imagen}
+            width='100%'
+            height={140}
+          />
+        </Card.Body>
+        <Card.Footer>
+          <Row justify='space-between'>
+            <Text transform='capitalize'>{name}</Text>
+            <Text>#{id + 1}</Text>
+          </Row>
+        </Card.Footer>
+      </Card>
+    </Grid>
+  )
+}
---
+++ pages/favorites/index.tsx
@@ -0,0 +1,12 @@
+import { Layout } from "../../components/layouts"
+
+const FavoritesPage = () => {
+  return (
+    <Layout title="Pokemons - Favoritos">
+      <h1>Favorites</h1>
+    </Layout>
+  )
+}
+
+
+export default FavoritesPage
